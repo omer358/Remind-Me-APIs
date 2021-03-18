6feaba4966ee1e65f3d2bf303c70ccad00f1da1f
omer358 <omermaki358@gmail.com>
2021-03-18 14:19:16 +0200

    connect to postgres database

--- RemindMe/RemindMe/settings.py
+++ RemindMe/RemindMe/settings.py
@@ -73,9 +73,12 @@ WSGI_APPLICATION = 'RemindMe.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Remind-Me',
+        'USER': 'omer358',
+        'PASSWORD': 'Thegre@tness358postgres',
+        'HOST': 'localhost'
     }
 }
 

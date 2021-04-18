84260a426feb59108437f15ae544ea9ba5965882
omer358 <omermaki358@gmail.com>
2021-04-18 05:05:29 +0200

    create person details

--- RemindMe/people/urls.py
+++ RemindMe/people/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('people/', views.PeopleList.as_view()),
-    path('people/<int:pk>', views.PersonDetails.as_view()),
+    path('people/<int:pk>/',views.PeopleDetails.as_view()),
     path('users/', views.UserList.as_view()),
 ]
--- RemindMe/people/views.py
+++ RemindMe/people/views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.models import User
+from django.http import HttpResponse, JsonResponse, Http404
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import generics, status
+from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import People
 from .serializers import PeopleSerializer, UserSerializer
@@ -14,15 +18,17 @@ class PeopleList(generics.ListCreateAPIView):
     def list(self, request, *args, **kwargs):
         if self.request.user.is_authenticated:
             queryset = People.objects.all().order_by('registration_time')
-            serializer_class = PeopleSerializer(queryset, many=True)
+            serializer_class = PeopleSerializer(queryset, many=True, context={'request': request})
+            print(request.build_absolute_uri())
             return Response(serializer_class.data)
         else:
             content = {'Authentication Error': 'Please Login!'}
             return Response(content, status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
-class PersonDetails(generics.GenericAPIView):
-    pass
+class PeopleDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = People.objects.all()
+    serializer_class = PeopleSerializer
 
 
 class UserList(generics.ListCreateAPIView):

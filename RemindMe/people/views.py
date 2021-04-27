f9c72a57f349ae8db3cfdea7d320725ea7f2ddcb
omer358 <omermaki358@gmail.com>
2021-04-27 02:34:05 +0200

    include the token in a param

--- RemindMe/people/views.py
+++ RemindMe/people/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework import generics, permissions
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.authtoken.models import Token
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -17,8 +18,8 @@ class PeopleList(generics.ListAPIView):
     authentication_classes = [SessionAuthentication, BasicAuthentication]
 
     def list(self, request, *args, **kwargs):
-        token = request.META['HTTP_AUTHORIZATION']
-        _id = Token.objects.get(key=token)
+        token = request.GET.get('token')
+        _id = get_object_or_404(Token, key=token)
         queryset = People.objects.all().filter(owner=_id.user_id).order_by('registration_time')
         serializer_class = PeopleSerializer(queryset, many=True, context={'request': request})
         return Response(serializer_class.data)
@@ -31,6 +32,13 @@ class PeopleDetails(generics.RetrieveUpdateAPIView):
     queryset = People.objects.all()
     serializer_class = PeopleSerializer
     authentication_classes = [SessionAuthentication, BasicAuthentication]
+
+    def retrieve(self, request, *args, **kwargs):
+        token = request.GET.get('token')
+        _id = get_object_or_404(Token, key=token)
+        queryset = People.objects.get(pk=_id.user_id)
+        serializer_class = PeopleSerializer(queryset, context={'request': request})
+        return Response(serializer_class.data)
 
 
 class UserList(generics.ListCreateAPIView):

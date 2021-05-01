b601043fa6f05a2ce374552d58650b2b79b0a774
omer358 <omermaki358@gmail.com>
2021-05-01 05:31:09 +0200

    create initial People model test

--- RemindMe/people/models.py
+++ RemindMe/people/models.py
@@ -6,12 +6,12 @@ from django.db import models
 class People(models.Model):
     first_name = models.CharField(max_length=15)
     second_name = models.CharField(max_length=15)
-    profile = models.ImageField(upload_to='profile_image',blank=True)
+    profile = models.ImageField(upload_to='profile_image', blank=True)
     meeting_place = models.CharField(max_length=60)
     meeting_time = models.DateTimeField()
     registration_time = models.DateTimeField(auto_now_add=True)
     notes = models.TextField(blank=True)
-    owner = models.ForeignKey('auth.User',on_delete=models.CASCADE, related_name='people')
+    owner = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='people')
 
     def __str__(self):
         return self.first_name + " " + self.second_name
--- RemindMe/people/tests.py
+++ RemindMe/people/tests.py
@@ -1,3 +1,35 @@
+from django.core.files import File
 from django.test import TestCase
+from rest_framework.authtoken.admin import User
 
-# Create your tests here.
+from .models import People
+
+
+class PeopleTest(TestCase):
+    """Test Module for People Model"""
+
+    def setUp(self) -> None:
+        user = User.objects.create(username='none', password='1234')
+        user.save()
+
+        People.objects.create(first_name='Omer',
+                              second_name='Maki',
+                              meeting_place='Home',
+                              meeting_time='2021-04-19 03:27:43+02',
+                              owner=user
+                              )
+        People.objects.create(first_name='Mohamed',
+                              second_name='Maki',
+                              meeting_place='Home',
+                              meeting_time='2021-04-29 03:27:43+02',
+                              owner=user
+                              )
+
+    def test_person(self):
+        omer_maki = People.objects.get(first_name='Omer')
+        mohamed_maki = People.objects.get(first_name='Mohamed')
+        self.assertEqual(
+            omer_maki.second_name, "Maki")
+        self.assertEqual(
+            mohamed_maki.second_name, "Maki")
+        self.assertEqual(People.objects.count(), 2)
